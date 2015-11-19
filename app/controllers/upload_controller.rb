7f2dad27cdd8817292d4db5168cfb897ddb7b3c2
xda-yoshioka <xda-yoshioka@tsm.kddilabs.jp>
2015-11-19 15:51:27 +0900

    #1を対応します。

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -1,14 +1,28 @@
+require 'json'
+
 class UploadController < ApplicationController
 
 protect_from_forgery except: :json
 
 def json
 	str = ""
-	params.each_key do |key|
-		str.concat(key)
-		str.concat("<br>")
-	end
-	render :text => params[:name]
+
+	# json tranfer
+	json_str = params[:json]
+	json_obj = JSON.parse(json_str)
+	str.concat("name:")
+	str.concat(json_obj["name"])
+
+	# upload info access
+	str.concat(",upload file name:")
+	upload_file = params[:upload1];
+	upload_file_data = upload_file.read
+	upload_file_name = upload_file.original_filename
+	str.concat(upload_file_name)
+	str.concat(",upload_file_size:")
+	str.concat("#{upload_file_data.size}")
+
+	render :text => str
 end
 
 end
